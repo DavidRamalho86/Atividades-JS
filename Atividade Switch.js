5a39af3888b4f68b2f83f31fde23767a6ebf9b0d
DavidRamalho86 <david.ramalho@aluno.senai.br>
2024-01-29 14:50:53 -0300

    clonando do antigo git

---
+++ Atividade Switch.js
@@ -0,0 +1,19 @@
+let estacaoDoAno = 'inverno' ;
+
+switch (estacaoDoAno) {
+		case 'verão':
+    		console.log ('Estamos no verão!');
+        break;
+    case 'inverno' :
+    		console.log ('Estamos no inverno!');
+        break ;
+    case 'outono' :
+    		console.log ('Estamos no outono!');
+        break ;
+    case 'primavera' :
+    		console.log ('Estamos no primavera!');
+        break ;
+    default:
+    		console.log ('Esta estação do ano não existe!');
+        break
+}
